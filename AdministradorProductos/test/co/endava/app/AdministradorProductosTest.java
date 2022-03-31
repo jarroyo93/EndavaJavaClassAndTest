d1d880ce47310b4a4e578c8ec736309f09ac4e2f
Jose Arroyo <ingjarroyo93@gmail.com>
2022-03-30 23:37:52 -0500

    Se agre un test que verifica el tipo de dato de un objeto

--- AdministradorProductos/test/co/endava/app/AdministradorProductosTest.java
+++ AdministradorProductos/test/co/endava/app/AdministradorProductosTest.java
@@ -1,33 +1,53 @@
 package co.endava.app;
 
 
+import co.endava.datos.Bebida;
 import co.endava.datos.Comida;
+import co.endava.datos.Producto;
 import org.junit.jupiter.api.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 
 
 class AdministradorProductosTest {
-
-    @BeforeAll
-    public void init() {
-
-    }
 
     @Test
     public void testDescuento() {
 
         double porcentajeControl = 10;
-        Comida p3 = new Comida(103, "Pastel", 2000, LocalDate.now().plusDays(2));
+        Comida p3 = new Comida(103, "Pastel", 2000, LocalDate.now());
         Double descuento = p3.getDescuento();
         Double percentProduct = descuento * 100 / p3.getPrecio();
 
         assertEquals(porcentajeControl, percentProduct);
     }
 
+    @Test
+    public void testTypeVar() {
+        Producto p2 = new Producto(102, "Azucar", 2500);
+
+        Object id = p2.getId();
+        Object nombre = p2.getNombre();
+        Object precio = p2.getPrecio();
+
+        String ide = id.getClass().toString();
+        String name = nombre.getClass().toString();
+        String price = precio.getClass().toString();
+
+        assertEquals("class java.lang.Integer", ide);
+        assertEquals("class java.lang.String", name);
+        assertEquals("class java.lang.Double", price);
+
+
+
+
+
+
+    }
 
 
 }
